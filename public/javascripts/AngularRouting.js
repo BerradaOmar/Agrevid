e6a221c6ca2783d3c9e09b6d3033705ab40d84a6
BERRADA <omar888ber@gmail.com>
2018-10-11 23:37:12 +0200

    commit & push dossier images & javascripts

---
+++ public/javascripts/AngularRouting.js
@@ -0,0 +1,24 @@
+var myApp = angular.module('routeApp',['ui.router']);
+
+myApp.config(function($stateProvider){
+
+    var homeState = {
+        name : 'accueil',
+        url : '/accueil',
+        template : '<h3>Page d\'acceuil</h3>'
+    };
+
+
+    var page1State = {
+        name : 'inscription',
+        url : '/inscription',
+        templateUrl : 'inscription.html',
+    };
+
+
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(page1State);
+
+
+});
